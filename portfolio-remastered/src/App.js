959c2616265d999862846372c670230a01781fce
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-09 14:26:11 -0400

    Importing routes into the Homepage

--- portfolio-remastered/src/App.js
+++ portfolio-remastered/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import "./index"
+import "./index.css";
+import Home from "./routes/HomePage";
+import Projects from "./routes/Projects";
+import ContactInfo from "./routes/ContactInfo";
+import Resume from "./routes/Resume";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    </div>
+<>
+<Routes>
+  <Route path="/" element={<Home />}> </Route>
+  <Route path="/Projects" element={<Projects />}> </Route>
+  <Route path="/ContactInfo" element={<ContactInfo />}> </Route>
+  <Route path="/Resume" element={<Resume />}> </Route>
+</Routes>
+</>
   );
 }
 
--- portfolio-remastered/src/routes/HomePage.js
+++ portfolio-remastered/src/routes/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home  = () => {
     return (
-        <div>About Me</div>
+        <div>JG's Portfolio</div>
     )
 }
 
