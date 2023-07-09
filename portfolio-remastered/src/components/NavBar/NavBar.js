bc5db2462b4e011558b0d99ac51a5b9674ecdf7e
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-09 18:03:15 -0400

    Adding in other components of my nav bar (ex: contact)

--- portfolio-remastered/src/components/NavBar/NavBar.js
+++ portfolio-remastered/src/components/NavBar/NavBar.js
@@ -8,6 +8,20 @@ const NavBar = () => {
         <Link to="/">
             <h1>JG's Portfolio</h1>
         </Link>
+        <ul>
+            <li>
+                <Link to="/">All About Me</Link>
+            </li>
+            <li>
+                <Link to="/Projects">Projects</Link>
+            </li>
+            <li>
+                <Link to="/ContactInfo">Contact</Link>
+            </li>
+            <li>
+                <Link to="/Resume">Resume</Link>
+            </li>
+        </ul>
     </div>
   )
 }
