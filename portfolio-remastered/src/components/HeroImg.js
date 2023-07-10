ee64f893f4b1503ec3cc7b7ca87c1db23ce6bdd6
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-09 20:50:25 -0400

    Adding in Main Image for website

---
+++ portfolio-remastered/src/components/HeroImg.js
@@ -0,0 +1,16 @@
+import "./HeroImg.css"; 
+
+
+import React from 'react'
+
+const Heroimg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+
+        </div>
+    </div>
+  )
+}
+
+export default Heroimg;
--- portfolio-remastered/src/routes/HomePage.js
+++ portfolio-remastered/src/routes/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar/NavBar";
+import HeroImg from "../HeroImg.js";
 
 const Home  = () => {
     return (
         <div>
         <NavBar/>
+        <HeroImg />
         </div>
     )
 }
