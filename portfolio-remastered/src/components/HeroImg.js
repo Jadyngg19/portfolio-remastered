527b02b5312c8a83ad0d81ccdd94048800bdb4e9
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-10 11:03:10 -0400

    Adding in Projects and Contact Buttons

--- portfolio-remastered/src/components/HeroImg.js
+++ portfolio-remastered/src/components/HeroImg.js
@@ -2,7 +2,8 @@ import "./HeroImg.css";
 
 import React from "react";
 
-import IntroImg from "../assests/HomePage.jpeg"
+import IntroImg from "../assests/HomePage.jpeg";
+import {Link} from "react-router-dom";
 
 const HeroImg = () => {
   return <div className="hero">
@@ -10,9 +11,14 @@ const HeroImg = () => {
     <img class="Intro-Img" src={IntroImg} alt="IntroImg"/>
     </div>
     <div className="content">
-    <p>I am a Junior Web Developer with a passion for coding and creating dynamic websites</p>
+    <p>I am a Junior Web Developer</p>
+    <h1>With a passion for coding and creating dynamic websites!</h1>
+    <div>
+    <Link to="/Projects" className="button">Projects</Link>
+    <Link to="/ContactInfo" className="button-light">Contact</Link>
     </div>
-  </div>;
+    </div>
+  </div>
 };
 
 export default HeroImg;
