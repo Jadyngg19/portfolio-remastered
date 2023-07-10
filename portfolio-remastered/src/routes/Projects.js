089df71a654943621664a287b3fa6ee1d74e52da
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-10 14:08:47 -0400

    Connecting main image file for projects page to the projects page

--- portfolio-remastered/src/routes/Projects.js
+++ portfolio-remastered/src/routes/Projects.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const Projects = () => {
   return (
     <div>
       <NavBar/>
+      <HeroImg2/>
       <Footer/>
     </div>
   )
