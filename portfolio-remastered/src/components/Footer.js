dbfb54822a558d5c838238d92f06402df76843d7
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-10 13:41:28 -0400

    Adding links to my social accounts via the social icons

--- portfolio-remastered/src/components/Footer.js
+++ portfolio-remastered/src/components/Footer.js
@@ -5,17 +5,40 @@ import React from 'react';
 import {FaFacebook, FaLinkedin, FaGithub} from "react-icons/fa";
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="socials">
-    <FaFacebook size={30} style={{color: "#FFBE0B", marginRight: "1rem"}}></FaFacebook>
-    <FaLinkedin size={30} style={{color: "#FFBE0B", marginRight: "1rem"}}></FaLinkedin>
-    <FaGithub size={30} style={{color: "#FFBE0B", marginRight: "1rem"}}></FaGithub>
-        </div>
-        </div>  
-    </div>
-  )
-}
 
-export default Footer
+    const handleFacebookClick = () => {
+        window.open('https://www.facebook.com/jadyn.gg19/', '');
+      };
+    
+      const handleLinkedInClick = () => {
+        window.open('https://www.linkedin.com/in/jadyn-gomez/', '');
+      };
+    
+      const handleGithubClick = () => {
+        window.open('https://github.com/Jadyngg19', '');
+      };
+
+      return (
+        <footer className="footer">
+          <div className="socials">
+            <FaFacebook
+              size={30}
+              style={{ color: "#FFBE0B", marginRight: "1rem", cursor: "pointer" }}
+              onClick={handleFacebookClick}
+            />
+            <FaLinkedin
+              size={30}
+              style={{ color: "#FFBE0B", marginRight: "1rem", cursor: "pointer" }}
+              onClick={handleLinkedInClick}
+            />
+            <FaGithub
+              size={30}
+              style={{ color: "#FFBE0B", marginRight: "1rem", cursor: "pointer" }}
+              onClick={handleGithubClick}
+            />
+          </div>
+        </footer>
+      );
+    };
+    
+    export default Footer;
