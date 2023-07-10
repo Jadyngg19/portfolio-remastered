bed33ffd4de111a58f918a5c9ef1856626463900
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-10 19:34:17 -0400

    Trying to get background for project page to render

--- portfolio-remastered/src/components/HeroImg2.js
+++ portfolio-remastered/src/components/HeroImg2.js
@@ -2,10 +2,13 @@ import "./HeroImg2.css";
 
 import React from 'react';
 
+import IntroImg2 from "../assets/Projects.jpg";
+
 const HeroImg2 = () => {
   return (
-    <div className="hero-img">
-        <div className="heading">
+    <div className="hero">
+            <img className="mask" src={IntroImg2} alt="IntroImg2"/>
+        <div className="content">
             <h1>Projects</h1>
             <p>Some of my most recent work</p>
         </div>
@@ -13,4 +16,4 @@ const HeroImg2 = () => {
   )
 }
 
-export default HeroImg2
+export default HeroImg2;
