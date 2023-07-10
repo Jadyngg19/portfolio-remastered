cd9c2ee5c55c9d8f68d3cf42acb07f94dd81dbec
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-10 19:47:30 -0400

    Fixing background image styling on projects page

--- portfolio-remastered/src/components/HeroImg2.js
+++ portfolio-remastered/src/components/HeroImg2.js
@@ -7,7 +7,9 @@ import IntroImg2 from "../assets/Projects.jpg";
 const HeroImg2 = () => {
   return (
     <div className="hero">
-            <img className="mask" src={IntroImg2} alt="IntroImg2"/>
+ <div className="mask">
+    <img className="intro-img" src={IntroImg2} alt="IntroImg2"/>
+    </div>
         <div className="content">
             <h1>Projects</h1>
             <p>Some of my most recent work</p>
