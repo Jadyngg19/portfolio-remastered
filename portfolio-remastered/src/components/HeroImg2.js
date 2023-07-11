bc4c9e3298143317abb1b2f2f8ab6cb51373ef6c
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-11 19:53:35 -0400

    Fixing styling of projects page

--- portfolio-remastered/src/components/HeroImg2.js
+++ portfolio-remastered/src/components/HeroImg2.js
@@ -6,14 +6,11 @@ import IntroImg2 from "../assets/Projects.jpg";
 
 const HeroImg2 = () => {
   return (
-    <div className="hero">
- <div className="mask">
-    <img className="intro-img" src={IntroImg2} alt="IntroImg2"/>
-    </div>
-        <div className="content">
-            <h1>Projects</h1>
-            <p>Some of my most recent work</p>
-        </div>
+    <div className="hero-img">
+      <div className="heading">
+        <h1>Projects</h1>
+        <p>Some of my most recent work</p>
+      </div>
     </div>
   )
 }
