ba2ebcacd3b7be97e5d1832cb284b4261bc186ab
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-11 19:26:25 -0400

    Adding in scroll feature where NavBar has a background color

--- portfolio-remastered/src/components/NavBar/NavBar.js
+++ portfolio-remastered/src/components/NavBar/NavBar.js
@@ -8,12 +8,24 @@ const NavBar = () => {
 const [click, setClick] = useState(false);
 const handleClick = () => setClick(!click);
 
+const [color, setColor] = useState(false);
+const changeColor = () =>{
+    if (window.scrollY >=100){
+        setColor(true);
+    }
+    else{
+        setColor(false);
+    }
+};
+
+window.addEventListener("scroll", changeColor);
+
   return (
-    <div className="header">
+    <div className={color ? "header header-bg": "header"}>
         <Link to="/">
             <h1 className="nav-bar-header">JG's Portfolio</h1>
         </Link>
-        <ul className={click ? "nav-menu.active" : "nav-menu"}>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">All About Me</Link>
             </li>
