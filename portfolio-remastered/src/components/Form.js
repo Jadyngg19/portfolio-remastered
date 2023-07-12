c4fb52b3c6b5ffe4d102ae511daca1f1e9e0b129
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-11 21:05:24 -0400

    Inputting contact form files and importing it on the contact page

---
+++ portfolio-remastered/src/components/Form.js
@@ -0,0 +1,11 @@
+import "./Form.css"
+
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form;
--- portfolio-remastered/src/routes/ContactInfo.js
+++ portfolio-remastered/src/routes/ContactInfo.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer";
-import HeroImg3 from '../components/HeroImg3';
+import HeroImg3 from "../components/HeroImg3";
+import Form from "../components/Form"
 
 const ContactInfo = () => {
   return (
     <div>
     <NavBar/>
     <HeroImg3/>
+    <Form/>
     <Footer/>
     </div>
   )
