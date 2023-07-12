9c4311f2af543e627f7ee08364b7ee99ed8f8ee3
Jadyn Gomez <jadyngg19@gmail.com>
2023-07-12 10:00:09 -0400

    Adding in and styling contact form

--- portfolio-remastered/src/components/Form.js
+++ portfolio-remastered/src/components/Form.js
@@ -4,7 +4,19 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="form">
+      <form>
+        <label>Your Name</label>
+        <input type="text"></input>
+        <label>Email Address</label>
+        <input type="email"></input>
+        <label>Subject</label>
+        <input type="text"></input>
+        <label>Message</label>
+        <textarea rows="6" placeholder="Type your message here and I will get back to you!"/>
+        <button className="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
