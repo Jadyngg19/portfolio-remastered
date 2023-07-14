fd43a4783edc0f05b1d308a961583bfc9ccbf041
Alex <cupokoffi@gmail.com>
2023-07-14 02:48:06 -0400

    Troubleshout page render error using HashRouter

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import { Routes, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Routes>
+      <Switch>
           <Route exact path="/" element={<Home />}> </Route>
           <Route path="/Projects" element={<Projects />}> </Route>
           <Route path="/ContactInfo" element={<ContactInfo />}> </Route>
           <Route path="/Resume" element={<Resume />}> </Route>
-      </Routes>
+      </Switch>
     </>
   );
 }
