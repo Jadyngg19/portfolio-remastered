88fe93f3fb76a3aaea650d23664410f61e8888da
Alex <cupokoffi@gmail.com>
2023-07-14 11:31:45 -0400

    BrowserRouter conflict with gh-pages - prioritize HashRouter

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Home from './routes/HomePage';
 import Projects from './routes/Projects';
 import ContactInfo from './routes/ContactInfo';
 import Resume from './routes/Resume';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/Projects" component={Projects} />
-      <Route path="/ContactInfo" component={ContactInfo} />
-      <Route path="/Resume" component={Resume} />
-    </Switch>
+    <HashRouter> 
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Projects" component={Projects} />
+        <Route path="/ContactInfo" component={ContactInfo} />
+        <Route path="/Resume" component={Resume} />
+      </Switch>
+    </HashRouter> 
   );
 }
 
