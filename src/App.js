0556aec08fefc56c2ddba363c9f7ef80f33672d0
Alex <cupokoffi@gmail.com>
2023-07-14 02:43:18 -0400

    Troubleshout page render error using HashRouter

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Home from "./routes/HomePage";
 import Projects from "./routes/Projects";
 import ContactInfo from "./routes/ContactInfo";
 import Resume from "./routes/Resume";
-import { HashRouter, Switch, Route } from 'react-router-dom'; 
+import { Routes, Switch, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <>
-      <HashRouter>
+      <Routes>
         <Switch>
-          <Route exact path="/" component={Home}> </Route>
-          <Route path="/Projects" component={Projects}> </Route>
-          <Route path="/ContactInfo" component={ContactInfo}> </Route>
-          <Route path="/Resume" component={Resume}> </Route>
+          <Route exact path="/" element={<Home />}> </Route>
+          <Route path="/Projects" element={<Projects />}> </Route>
+          <Route path="/ContactInfo" element={<ContactInfo />}> </Route>
+          <Route path="/Resume" element={<Resume />}> </Route>
         </Switch>
-      </HashRouter>
+      </Routes>
     </>
   );
 }
