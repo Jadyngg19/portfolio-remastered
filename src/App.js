1d42e062b800ad8d8f9414068457ff15fd5bc414
Alex <cupokoffi@gmail.com>
2023-07-14 11:27:12 -0400

    Revert to BR and use Switch instead of Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from "react";
-import "./index.css";
-import Home from "./routes/HomePage";
-import Projects from "./routes/Projects";
-import ContactInfo from "./routes/ContactInfo";
-import Resume from "./routes/Resume";
-import { Routes, Switch, Route } from 'react-router-dom'; 
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import React from 'react';
+import './index.css';
+import Home from './routes/HomePage';
+import Projects from './routes/Projects';
+import ContactInfo from './routes/ContactInfo';
+import Resume from './routes/Resume';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-            <Route exact path="/" element={<Home />}> </Route>
-            <Route path="/Projects" element={<Projects />}> </Route>
-            <Route path="/ContactInfo" element={<ContactInfo />}> </Route>
-            <Route path="/Resume" element={<Resume />}> </Route>
-        </Switch>
-      </BrowserRouter>
-    </>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/Projects" component={Projects} />
+      <Route path="/ContactInfo" component={ContactInfo} />
+      <Route path="/Resume" component={Resume} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'; 
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom"
 
-ReactDOM.render(
-  <HashRouter>
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+root.render(
+  <BrowserRouter>
     <App />
-  </HashRouter>,
-  document.getElementById("root") 
+  </BrowserRouter>
 );
