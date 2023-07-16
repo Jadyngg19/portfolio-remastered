c0476b0a48e20af2a2544520b70bc3568c8d988f
Alex <cupokoffi@gmail.com>
2023-07-15 20:57:16 -0400

    Fixing links to early works

--- src/components/EarlyWorks/index.js
+++ src/components/EarlyWorks/index.js
@@ -14,7 +14,7 @@ const ExtraProjs = () => {
                 <p>- Five-Day Forecast -</p>
                 <p>- Allows you to see your past searches -</p>
                 <p>- Shows the Temp, Wind, and Hunidity -</p>
-                <NavLink to="https://github.com/Jadyngg19/What-Is-The-Weather-Like" className="button">View Source</NavLink>
+                <a href="https://github.com/Jadyngg19/What-Is-The-Weather-Like" className="button">View Source</a>
             </div>
             <div className="card">
                 <h3>- Early Works -</h3>
@@ -23,7 +23,7 @@ const ExtraProjs = () => {
                 <p>- Back-end project -</p>
                 <p>- Includes prompts to provide information -</p>
                 <p>- Allows you to put a license logo -</p>
-                <NavLink to="https://github.com/Jadyngg19/Create-Me-A-README.md-File" className="button">View Source</NavLink>
+                <a href="https://github.com/Jadyngg19/Create-Me-A-README.md-File" className="button">View Source</a>
             </div>
             <div className="card">
                 <h3>- Early Works -</h3>
@@ -32,7 +32,7 @@ const ExtraProjs = () => {
                 <p>- Includes symbols and numbers -</p>
                 <p>- Prompts response to customize password -</p>
                 <p>- Has a generate button -</p>
-                <NavLink to="https://github.com/Jadyngg19/PasswordGenerator_2" className="button">View Source</NavLink>
+                <a href="https://github.com/Jadyngg19/PasswordGenerator_2" className="button">View Source</a>
             </div>
         </div>
     </div>
